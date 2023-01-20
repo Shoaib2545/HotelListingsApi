766dd1616622037f458cf486cf01b72ab20ee794
Muhammad Shoaib <md.shoaib2545@gmail.com>
2023-01-20 23:05:22 +0500

    Get country by id api created

--- HotelListings/Controllers/CountryController.cs
+++ HotelListings/Controllers/CountryController.cs
@@ -22,12 +22,27 @@ namespace HotelListings.Controllers
         public async Task<IActionResult> GetCountries() {
             try {
                 var countries = await _unitOfWork.Countries.GetAll();
-                var result = _mapper.Map<IList<CountryDTO>>(countries);
-                return Ok(result);
+                var results = _mapper.Map<IList<CountryDTO>>(countries);
+                return Ok(results);
             }
             catch(Exception ex) {
                 _logger.LogError(ex,$"Something went wrong in the{nameof(GetCountries)}");
                 return StatusCode(500,"Internal Server Error. Something Went Wrong Please Try Again Later.");
+            }
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCountry(int id)
+        {
+            try
+            {
+                var country = await _unitOfWork.Countries.Get(q => q.Id == id, new List<string>{ "Hotels" });
+                var result = _mapper.Map<CountryDTO>(country);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong in the{nameof(GetCountry)}");
+                return StatusCode(500, "Internal Server Error. Something Went Wrong Please Try Again Later.");
             }
         }
     }
